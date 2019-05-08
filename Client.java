8e2861240ba7d25f2b7f504582ef8e4c6ff4543a
Bharti sharma <bharti160599@gmail.com>
2019-05-08 20:52:08 +0530

    typing master added

---
+++ Client.java
@@ -0,0 +1,9 @@
+package game;
+
+public class Client {
+
+	public static void main(String[] args) {
+		TypingTutor obj = new TypingTutor("b h a r t i s h a r m a m o n i k a s h a r m a s u m e s h s h a r m a a b  d e f g h i j k l m n o p q r s t u r w x y z");
+	}
+
+}
